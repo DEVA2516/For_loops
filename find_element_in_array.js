25f052bab22e14a0e91d6391857234c59602765f
Your Name <you@example.com>
2022-07-23 20:06:46 +0530

    find

--- find_element_in_array.js
+++ find_element_in_array.js
@@ -1,15 +1,24 @@
 const array = [23,13,10,78,12,15,20];
-b = 10;
-Flag = 0;
-for (let i=0;i<array.length;i++){
-    if(array[i]==b){
-        console.log("Found");
-        Flag = 1;
-        break;
+let b = prompt();
+let Flag = 0;
+if((b>=0)&&(!isempty(b))) {
+    for (let i=0;i<array.length;i++){
+        if(array[i]==b){
+            console.log("Found");
+            Flag = 1;
+            break;
+        }
+        Flag = 0;
     }
-    Flag = 0;
-}
 
-if(Flag == 0){
-    console.log("Not Found");
+    if(Flag == 0)
+        console.log("Not Found");
+    
+}
+else 
+    console.log("Invalid number......Try number b>=0");
+
+
+function isempty(value) {
+    return (value==null||value == ' '||value.length <= 0||value === undefined)?true:false;
 }
