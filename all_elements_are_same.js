c6cd4790dcac029ecc0fd317cf69eeb469e69fc9
Your Name <you@example.com>
2022-07-23 15:32:01 +0530

    sort

---
+++ all_elements_are_same.js
@@ -0,0 +1,17 @@
+const array = [2,1,1,1];
+let b = prompt();
+if(b>=0){
+let count = 0;
+for(let i=0;i<array.length;i++){
+    if(array[i]!=b){
+        console.log("All elements are not same........");
+        break;
+    }
+    count++;
+}
+
+if(count == array.length)
+    console.log("All elements are same....");
+}
+else 
+    console.log("invalid element..........Try valid element >=0!!!!!!!!!!!!!!!!!!!");s
