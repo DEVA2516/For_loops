777ed01f7ee35a61ec7107a6947156126c017ea1
Your Name <you@example.com>
2022-07-29 00:00:52 +0530

    pattern_pyramid...

--- sum_series_ex7.js
+++ sum_series_ex7.js
@@ -3,16 +3,16 @@ is = require("./isem.js");
 let x = prompt("",2);
 let num = prompt("",5);
 
-if(x>0 && num>0 && !is.isempty(x) && !is.isempty(num)){
+if(!is.isempty(x) && !is.isempty(num)){
 
     let sum = 0;
     let j=1;
 
     for(i=0;i<num;i++){
         if(i%2==0)
-            sum += x ** (j);
+            sum += x ** j;
         else
-            sum -= x ** (j);
+            sum -= x ** j;
             //console.log(sum);
 
         j+=2;
@@ -21,11 +21,3 @@ if(x>0 && num>0 && !is.isempty(x) && !is.isempty(num)){
 }
 else
     console.log("Invlid range........check.....min.....max");
-
-
-function fact(num){
-    let ft=1;
-    for(let i=1;i<=num;i++)
-        ft = ft*i;
-    return ft;
-}
