e69eb746dae4bad2ef78a333747bca6a264b59c3
Your Name <you@example.com>
2022-08-02 10:42:09 +0530

    sum series

--- sum_of_series_ex_10.js
+++ sum_of_series_ex_10.js
@@ -1,3 +1,14 @@
-let num = 0;
+let num = 10;
 let diff=1.1;
-let sum = 0.0;
+let sum = 0.1;
+let sumseries = 0;
+
+for(let i=0;i<num;i++){
+    sum += diff;
+    console.log(parseFloat(sum).toFixed(2));
+    sumseries += sum; 
+}
+console.log("....................");
+
+console.log("sum of series = "+ sumseries);
+//pa
