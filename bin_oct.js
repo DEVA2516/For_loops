09414fe0999c55e09f6ab18490c1fea90d31cab2
Your Name <deva162501@gmail.com>
2022-08-02 20:06:47 +0530

    octal

--- bin_oct.js
+++ bin_oct.js
@@ -1,11 +1,21 @@
-let bin = 10111;
-let oct = 0;
+let bin =10010111;
+let dec = 0;
 let j = 0;
 
 for(let i=bin;i>0;i=parseInt(bin)){
     let rem = i % 10;
-    oct += rem * Math.pow(8,j++);
+    dec += rem * Math.pow(2,j++);
     bin /= 10;
 }
 
+console.log(dec);
+let oct = 0;
+let k = 1;
+for(let i=dec;i>0;i=parseInt(dec)){
+    let rem = i % 8;
+    oct += rem * k;
+    k *= 10;
+    dec /= 8;
+
+}
 console.log(oct);
