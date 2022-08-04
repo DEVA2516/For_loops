ea5da9d2e3aecef2d875d52bd53b8baf3c140410
Your Name <deva162501@gmail.com>
2022-08-04 14:46:53 +0530

    prime number

--- prime_number.js
+++ prime_number.js
@@ -1,28 +1,30 @@
-let number = prompt("prime",23);
+// 5. Write a program to check whether a number is prime or not.
 
-if(number >= 2 && (!isempty(number))){
+let number = prompt("prime", 23);
+
+if (number >= 2 && (!isempty(number))) {
     let flag = 1;
-    for(let i=2;i<=Math.sqrt(number);i++){
-        if(number % i == 0) {
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i == 0) {
             flag = 0;
             console.log(number + " is not prime");
             break;
         }
-         flag = 1;   
+        flag = 1;
     }
 
-    if(flag == 1) {
-        console.log(number+" is prime");
+    if (flag == 1) {
+        console.log(number + " is prime");
     }
 }
-else if ((number==0||number == 1) && (!isempty(number)))
+else if ((number == 0 || number == 1) && (!isempty(number)))
     alert(number + " is not prime.......");
 
 
-else 
+else
     alert("Invlid number......try valid number>0");
 
 
-function isempty(value){
-    return (value === undefined||value == null||value == ''||value==' ')?true:false;
+function isempty(value) {
+    return (value === undefined || value == null || value == '' || value == ' ') ? true : false;
 }
