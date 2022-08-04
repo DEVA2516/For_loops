f339cba95e5068f11cb12e90ddc845352ce31001
Your Name <deva162501@gmail.com>
2022-08-04 14:45:14 +0530

    perfect number

--- perfect_number.js
+++ perfect_number.js
@@ -1,21 +1,23 @@
-let min = prompt("min = ",1);
-let max = prompt("max = ",500);
+// 4. Write a program to find the perfect numbers between 1 and 500.
 
-if((min>0)&&(min<max)&&(!isempty(min))&&(!isempty(max))){
-    for(let num=min;num<=max;num++){
+let min = prompt("min = ", 1);
+let max = prompt("max = ", 500);
+
+if ((min > 0) && (min < max) && (!isempty(min)) && (!isempty(max))) {
+    for (let num = min; num <= max; num++) {
         let sum = 0;
         //console.log(num);
-        for(let i = 1;i<num;i++){
-            if(num % i == 0)
+        for (let i = 1; i < num; i++) {
+            if (num % i == 0)
                 sum += i;
         }
-        if(sum == num)
+        if (sum == num)
             console.log(num);
     }
 }
-else 
+else
     console.log("Invalid range ......check ...min and max...........");
 
-function isempty(value){
-    return (value == null || value == '' || value === undefined)?true:false;
+function isempty(value) {
+    return (value == null || value == '' || value === undefined) ? true : false;
 }
