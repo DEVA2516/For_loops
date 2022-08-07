dfb35eac7a1c2eee674848109de9cb66d8a400e8
Your Name <deva162501@gmail.com>
2022-08-07 23:58:38 +0530

    pyramid_of_n_numbers_column_in_1st_row......

---
+++ pattern_odd_row_reverse_column.js
@@ -0,0 +1,24 @@
+// 55. Write a program to display such a pattern for n number of rows using
+// number. Each row will contain odd numbers of number. The first and last number
+// of each row will be 1 and middle c
+
+let num = 7;
+let str = '';
+
+for (let i = num; i > 0; i--) {
+
+    let k = 1;
+
+    for (l = num - i; l >= 0; l--) {
+        str += ' ';
+    }
+    for (let j = 1; j <= 2 * i - 1; j++) {
+        if (j < i) {
+            str += k++ + ' ';
+        }
+        else
+            str += k-- + ' ';
+    }
+    console.log(str);
+    str = '';
+}
