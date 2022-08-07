6940a62e7f409e8f1807a9e0a32e94cccfe1dd26
Your Name <deva162501@gmail.com>
2022-08-08 00:18:39 +0530

    factorial of number ......

--- factorial.js
+++ factorial.js
@@ -1,14 +1,16 @@
+// 7. Write a program to find the factorial of a number.
+
 let num = prompt();
 
-if(num>0 && !isempty(num)){
+if (num > 0 && !isempty(num)) {
     let fact = 1;
-    for(let i =1;i<=num;i++)
+    for (let i = 1; i <= num; i++)
         fact = fact * i;
     console.log(fact);
 }
 else
     alert("Invalid number.......try number greater than zero...");
 
-    function isempty(value){
-        return (value==''||value==null||value===undefined)?true:false;
-    }
+function isempty(value) {
+    return (value == '' || value == null || value === undefined) ? true : false;
+}
