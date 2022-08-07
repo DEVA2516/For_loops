b01c30d014091253e46a2afb0f7e1ba7bc67af68
Your Name <deva162501@gmail.com>
2022-08-08 00:28:57 +0530

    Sum_of_digit_of_given_number ......

--- sum_of_digits.js
+++ sum_of_digits.js
@@ -1,20 +1,20 @@
-//10. Write a program to find the sum of digits of a given number.
+// 10. Write a program to find the sum of digits of a given number.
 
 let num = prompt();
 
-if(num>0&&(!isempty(num))){
+if (num > 0 && (!isempty(num))) {
     let sum = 0;
-    for(i=num;i>0;i=parseInt(num)){
+    for (i = num; i > 0; i = parseInt(num)) {
         let rem = i % 10
-        sum = sum + rem; 
-        num /=10;
+        sum = sum + rem;
+        num /= 10;
         //console.log(sum);
     }
-    console.log("sum = "+sum);
+    console.log("sum = " + sum);
 }
 else
     alert("Invlid digit.........Try num greater than zero.......");
 
-function isempty(value){
-        return (value == null||value === undefined||value=='')?true:false;
-    }
+function isempty(value) {
+    return (value == null || value === undefined || value == '') ? true : false;
+}
