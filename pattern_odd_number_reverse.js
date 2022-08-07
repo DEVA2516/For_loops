21fa2da657e8debc43a077cc3697e7ca490e7706
Your Name <deva162501@gmail.com>
2022-08-07 22:57:05 +0530

    pattern_middle column_ row number

---
+++ pattern_odd_number_reverse.js
@@ -0,0 +1,19 @@
+// 47. Write a program to display such a pattern for n number of rows using
+// number.Each row will contain odd numbers of number.The first and last number
+// of each row will be 1 and middle column will be the row number.
+
+let num = 5;
+let str = '';
+
+for (let i = 1; i <= num; i++) {
+    let k = 1;
+    for (let j = 1; j <= 2 * i - 1; j++) {
+        if (j < i) {
+            str += k++ + ' ';
+        }
+        else
+            str += k-- + ' ';
+    }
+    console.log(str);
+    str = '';
+}
