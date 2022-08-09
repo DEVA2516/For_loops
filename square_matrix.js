1d264e2209869f85454de4558f233fe2266e387f
Your Name <deva162501@gmail.com>
2022-08-09 08:29:53 +0530

    square_matrix_main diagonal 0's down

--- square_matrix.js
+++ square_matrix.js
@@ -3,7 +3,7 @@ let str = '';
 
 for (let i = 0; i < num; i++ ) {
     let k = i;
-    let l = 0
+    let l = 0;
     
     for (let j = 0; j < num ; j++)
             str += (k>0)? k-- + ' ' : l++  + ' ';
