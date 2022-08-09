c5d6c685d911d5463572611b459813f44fd512bd
Your Name <deva162501@gmail.com>
2022-08-09 08:27:50 +0530

    square_matrix_with 0's main diagonal....

---
+++ square_matrix.js
@@ -0,0 +1,13 @@
+let num = 5;
+let str = '';
+
+for (let i = 0; i < num; i++ ) {
+    let k = i;
+    let l = 0
+    
+    for (let j = 0; j < num ; j++)
+            str += (k>0)? k-- + ' ' : l++  + ' ';
+    
+    console.log(str);
+    str = '';
+}
